540955731175f8167d103ab3da4276c1f8aa7d02
nlkassad <nlkassad@hotmail.com>
2016-09-27 14:50:05 -0400

    Remove Article/Comments Add Feature and Unit Tests
    Add testing for features

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  get '/articles' => 'articles#index'
+  # resources :articles, only: [:index]
 end
