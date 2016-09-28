4fcf7f56f22cdbf53f76453eaf86ea8af6ee5585
nlkassad <nlkassad@hotmail.com>
2016-09-28 13:45:45 -0400

    Add Article Comments Feature
    Add articles with spec test to handle associating comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+  # rspec ./spec/models/article_spec.rb:24
+  validates :content, presence: true
+  validates :title, presence: true
+  # validates_presence_of :content
+
+  has_many :comments, inverse_of: :article, dependent: :destroy
 end
